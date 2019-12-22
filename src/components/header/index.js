3b0903a4d2ca82a49f3ddf819f0d08be66b6a30f
Daniel Hess <dan9186@gmail.com>
2019-12-22 02:33:48 -0800

    removing menu button until needed

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -7,14 +7,9 @@ import { makeStyles } from '@material-ui/core/styles'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
-import IconButton from '@material-ui/core/IconButton'
-import MenuIcon from '@material-ui/icons/Menu'
 import Button from '@material-ui/core/Button'
 
 const useStyles = makeStyles(theme => ({
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
   title: {
     flexGrow: 1,
   },
@@ -40,14 +35,6 @@ export const Header = () => {
   return (
     <AppBar position='static'>
       <Toolbar>
-        <IconButton
-          edge='start'
-          color='inherit'
-          aria-label='menu'
-          className={classes.menuButton}
-        >
-          <MenuIcon />
-        </IconButton>
         <Typography variant='h6' className={classes.title}>
           Docket
         </Typography>
