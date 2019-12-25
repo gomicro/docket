9df1d4a8dd1a955420a1d324f41190b84aa6259c
Daniel Hess <dan9186@gmail.com>
2019-12-24 23:17:13 -0800

    some style to the 404

--- src/pages/not_found/index.js
+++ src/pages/not_found/index.js
@@ -1,3 +1,42 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-export const NotFound = () => <h1>Not Found</h1>
+import { makeStyles } from '@material-ui/core/styles'
+
+import { Card, CardContent, CardHeader, Typography } from '@material-ui/core'
+
+const useStyles = makeStyles({
+  card: {
+    maxWidth: 450,
+    margin: '6em auto',
+
+    '& .MuiCardHeader-content': {
+      textAlign: 'center',
+    },
+  },
+  cardContent: {
+    textAlign: 'center',
+  },
+})
+
+export const NotFound = () => {
+  const classes = useStyles()
+
+  return (
+    <Card className={classes.card}>
+      <CardHeader title='404 Not Found' />
+      <CardContent>
+        <Typography
+          className={classes.cardContent}
+          variant='body2'
+          color='textSecondary'
+          component='p'
+        >
+          By now we've guessed that you don't know where you are. The catch is
+          that we don't know where you are either. Enjoy your time in knowere
+          and we'll see you at <Link to={'/'}>Home</Link>.
+        </Typography>
+      </CardContent>
+    </Card>
+  )
+}
