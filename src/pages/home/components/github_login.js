8d960f2b3ff265756904f6a16bd17cd879e16b7c
Daniel Hess <dan9186@gmail.com>
2020-01-04 15:45:45 -0800

    create github login component

---
+++ src/pages/home/components/github_login.js
@@ -0,0 +1,63 @@
+import React from 'react'
+
+import { Auth } from '../../../clients/github'
+
+import { makeStyles } from '@material-ui/core/styles'
+import {
+  Card,
+  CardActions,
+  CardContent,
+  Button,
+  TextField,
+} from '@material-ui/core'
+
+const useStyles = makeStyles(theme => ({
+  card: {
+    maxWidth: 350,
+    margin: '4em auto',
+  },
+  authForm: {
+    display: 'flex',
+    '& .MuiTextField-root': {
+      flexGrow: 1,
+      margin: theme.spacing(1),
+    },
+  },
+  loginButton: {
+    flexGrow: 1,
+    margin: theme.spacing(1),
+  },
+}))
+
+export const GithubLogin = ({ setAuthed }) => {
+  const classes = useStyles()
+
+  const handleAuth = () => {
+    const token = document.getElementById('github-auth-token').value
+    setAuthed(Auth.setToken(token))
+  }
+
+  return (
+    <Card className={classes.card}>
+      <CardContent>
+        <form className={classes.authForm} noValidate autoComplete='off'>
+          <TextField
+            required
+            id='github-auth-token'
+            label='Github Auth Token'
+          />
+        </form>
+      </CardContent>
+      <CardActions>
+        <Button
+          className={classes.loginButton}
+          variant='contained'
+          color='primary'
+          onClick={handleAuth}
+        >
+          Login
+        </Button>
+      </CardActions>
+    </Card>
+  )
+}
