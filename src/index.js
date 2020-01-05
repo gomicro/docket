4cbb0cf704553ce512a7dbafe3ffd2af8eee4473
Daniel Hess <dan9186@gmail.com>
2020-01-05 02:26:20 -0800

    move the error boundary up as high as we can

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 
 import { App } from './app'
+import { ErrorBoundary } from './error_boundary'
 
 ReactDOM.render(
   <BrowserRouter forceRefresh={true}>
-    <App />
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
   </BrowserRouter>,
   document.getElementById('root'),
 )
