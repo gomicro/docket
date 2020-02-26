c54d1643d0738a103b39186886acc34fdc1d4360
Daniel Hess <dan9186@gmail.com>
2020-02-25 17:46:29 -0800

    rework pr list item to show status and updated appearance for next changes

--- src/pages/home/components/pr_list.js
+++ src/pages/home/components/pr_list.js
@@ -1,57 +1,13 @@
 import React from 'react'
-import moment from 'moment'
 
-import {
-  Chip,
-  List,
-  ListItem,
-  ListItemIcon,
-  ListItemSecondaryAction,
-  ListItemText,
-} from '@material-ui/core'
-import { MergeType } from '@material-ui/icons'
+import { PRListItem } from './pr_list_item'
 
-const chipVal = date => {
-  const from = moment(`${date}`)
-  const now = moment(moment.now())
-  const diff = now.diff(from, 'seconds')
-
-  if (now.diff(from, 'seconds') < 60) {
-    return `${diff}s`
-  } else if (now.diff(from, 'seconds') < 3600) {
-    return `${now.diff(from, 'minutes')}m`
-  } else if (now.diff(from, 'seconds') < 86400) {
-    return `${now.diff(from, 'hours')}h`
-  } else if (now.diff(from, 'seconds') < 604800) {
-    return `${now.diff(from, 'day')}d`
-  } else if (now.diff(from, 'seconds') < 2419200) {
-    return `${now.diff(from, 'week')}w`
-  } else if (now.diff(from, 'seconds') < 13222801) {
-    return `${now.diff(from, 'month')}M`
-  } else {
-    return `${now.diff(from, 'year')}y`
-  }
-}
+import { List } from '@material-ui/core'
 
 export const PRList = ({ prs = [] }) => (
   <List dense disablePadding>
     {prs.map(pr => (
-      <ListItem
-        dense
-        key={`${pr.org}:${pr.repo}:${pr.id}`}
-        button
-        component='a'
-        href={pr.link}
-        target='_blank'
-      >
-        <ListItemIcon>
-          <MergeType />
-        </ListItemIcon>
-        <ListItemText primary={pr.title} secondary={`${pr.org}/${pr.repo}`} />
-        <ListItemSecondaryAction>
-          <Chip label={chipVal(pr.createdAt)} />
-        </ListItemSecondaryAction>
-      </ListItem>
+      <PRListItem key={`${pr.org}:${pr.repo}:${pr.id}`} pr={pr} />
     ))}
   </List>
 )
---
+++ src/pages/home/components/pr_list_item.js
@@ -0,0 +1,95 @@
+import React, { useContext, useEffect, useState } from 'react'
+import moment from 'moment'
+
+import { Context } from 'context'
+import { PullRequests } from '../../../clients/github'
+
+import { styled } from '@material-ui/core/styles'
+import { MergeType, LibraryBooks } from '@material-ui/icons'
+import {
+  Avatar,
+  Badge,
+  IconButton,
+  ListItem,
+  ListItemAvatar,
+  ListItemSecondaryAction,
+  ListItemText,
+} from '@material-ui/core'
+
+const MergeFeatureFlag = false
+
+const chipVal = date => {
+  const from = moment(`${date}`)
+  const now = moment(moment.now())
+  const diff = now.diff(from, 'seconds')
+
+  if (now.diff(from, 'seconds') < 60) {
+    return `${diff}s`
+  } else if (now.diff(from, 'seconds') < 3600) {
+    return `${now.diff(from, 'minutes')}m`
+  } else if (now.diff(from, 'seconds') < 86400) {
+    return `${now.diff(from, 'hours')}h`
+  } else if (now.diff(from, 'seconds') < 604800) {
+    return `${now.diff(from, 'day')}d`
+  } else if (now.diff(from, 'seconds') < 2419200) {
+    return `${now.diff(from, 'week')}w`
+  } else if (now.diff(from, 'seconds') < 13222801) {
+    return `${now.diff(from, 'month')}M`
+  } else {
+    return `${now.diff(from, 'year')}y`
+  }
+}
+
+export const PRListItem = ({ pr }) => {
+  const [status, setStatus] = useState('unapproved')
+  const { addAlert } = useContext(Context)
+
+  useEffect(() => {
+    if (pr) {
+      PullRequests.getReviewStatus(pr)
+        .then(statuses => statuses.map(status => status.state))
+        .then(statuses => {
+          if (statuses.includes('CHANGES_REQUESTED')) {
+            setStatus('needsChanges')
+          } else if (statuses.includes('APPROVED')) {
+            setStatus('approved')
+          }
+        })
+        .catch(error => addAlert(error.toString()))
+    }
+  }, [pr])
+
+  return (
+    <ListItem dense button component='a' href={pr.link} target='_blank'>
+      <ListItemAvatar>
+        <Badge
+          color='secondary'
+          overlap='circle'
+          badgeContent={chipVal(pr.createdAt)}
+        >
+          <StyledAvatar status={status}>
+            <LibraryBooks />
+          </StyledAvatar>
+        </Badge>
+      </ListItemAvatar>
+      <ListItemText primary={pr.title} secondary={`${pr.org}/${pr.repo}`} />
+      {MergeFeatureFlag && (
+        <ListItemSecondaryAction>
+          <IconButton edge='end' aria-label='delete'>
+            <MergeType />
+          </IconButton>
+        </ListItemSecondaryAction>
+      )}
+    </ListItem>
+  )
+}
+
+const StyledAvatar = styled(Avatar)`
+  ${({ status }) => {
+    if (status === 'approved') {
+      return 'background-color: green;'
+    } else if (status === 'needsChanges') {
+      return 'background-color: red;'
+    }
+  }}
+`
